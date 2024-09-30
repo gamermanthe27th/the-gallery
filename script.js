1c13a512cd3221b06e45a389863899a2af3ff45a
gamermanthe27th <jpbelinhuchak@gmail.com>
2024-09-30 16:23:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+function info_mona() {
+    alert("Mona Lisa (Senhora Lisa) também conhecida como A Gioconda (em italiano: La Gioconda, a sorridente, em francês, La Joconde) ou ainda Mona Lisa del Giocondo (Senhora Lisa esposa de Giocondo) é a mais notável e conhecida obra de Leonardo da Vinci, um dos mais eminentes homens do Renascimento italiano. Sua pintura foi iniciada em 1503 (terminando-a três ou quatro anos mais tarde) e é nesta obra que o artista melhor concebeu a técnica do sfumato. O quadro representa uma mulher com uma expressão introspectiva e um pouco tímida. O seu sorriso restrito é muito sedutor, mesmo que um pouco conservador. O seu corpo representa o padrão de beleza da mulher na época de Leonardo. Este quadro é provavelmente o retrato mais famoso na história da arte, senão o quadro mais famoso e valioso de todo o mundo. Poucos outros trabalhos de arte são tão controversos, questionados, valiosos, elogiados, comemorados ou reproduzidos. Muitos historiadores da arte acreditam que a reverência de Leonardo da Vinci pela Mona Lisa nada tinha a ver com sua maestria artística; segundo muitos, devia-se a algo muito mais profundo: uma mensagem oculta nas camadas da pintura. A linha do horizonte à esquerda encontra-se num nível visivelmente mais baixo que a da direita, fazendo com que a Mona Lisa pareça muito maior vista da esquerda que da direita. Historicamente, os conceitos de masculino e feminino estão ligados aos lados — o esquerdo é feminino, o direito é o masculino. A pintura a óleo sobre madeira de álamo encontra-se exposta no Museu do Louvre, em Paris, e é uma das suas maiores atrações. ");
+    }
+
+    function quiz_mona() {
+        var resposta = confirm("quer fazer o quiz?");
+        
+        if (resposta) {
+            var mona_paint = prompt("Quem fez a Mona Lisa?");
+                alert("o criador da Mona Lisa Foi o Leonardo Da Vinci, durante os anos de 1503 e 1506");
+            close;    
+        }
+        }
+        
+        function info_adam() {
+            alert("A Criação de Adão é um afresco de 280 cm x 570 cm, pintado por Michelangelo Buonarroti por volta de 1511, que fica no teto da Capela Sistina. A cena representa um episódio do Livro do Gênesis no qual Deus cria o primeiro homem a partir do pó da terra: Adão. Deus é representado como um ancião barbudo, envolto em um manto que divide com alguns anjos. Seu braço esquerdo está abraçado a uma figura feminina, normalmente interpretada como Adão e Eva – que ainda não foi criada e, figuradamente, espera no Céu para ganhar uma forma humana. O braço direito de Deus está esticado para criar o poder da vida de seu próprio dedo para Adão, o qual está com o braço esquerdo estendido em contraposição ao do criador. Os dedos de Adão e de Deus estão separados por uma pequena distância. A composição é obviamente artística e não literal, já que Adão é capaz de alcançar Deus mesmo antes de ter ganho vida. Pela mesma razão, Eva é vista representada antes de sua própria criação. Esse motivo levou algumas pessoas a acreditar que a figura feminina fosse a suposta primeira esposa de Adão, Lilith; entretanto, essa interpretação não faz o menor sentido, já que, em sua lenda, Lilith também teria sido criada depois de Adão. As posições de Deus e Adão, a pintura do braço direito de Deus e esquerdo de Adão são quase idênticas e representam o fato de que, como diz o Gênesis 1,27, Deus criou o homem à sua imagem e semelhança. O dedo indicador de Adão, a mais famosa representação do afresco, não é de facto um trabalho de Michelangelo. Ele foi danificado durante reparos de um desabamento em meados do século XVI e foi reprimido e repintado por um restaurador do Vaticano. Por outra perspectiva, alguns estudiosos das artes dizem que uma mensagem é imposta subliminarmente. Quando, olhado de forma diferente, Deus estaria representado dentro de um cérebro e, com o seu toque à Adão, o racionalismo seria representado de forma escondida. ");}
+        
+            function quiz_adam() {
+                var resposta = confirm("quer fazer o quiz?");
+        
+        if (resposta) {
+            var adam_paint = prompt("Quem fez A Criação de Adão?");
+                alert("o criador da Criação de Adão foi Michelangelo Buonarroti entre os anos 1508 e 1512");
+            close;    
+        }
+        }
+                function info_jesus() {
+                    alert("A Última Ceia (em italiano: L'Ultima Cena e também Il Cenacolo) é um afresco de Leonardo da Vinci para a igreja de Santa Maria delle Grazie em Milão, Itália. O trabalho presume-se que tenha sido iniciado por volta de 1495-96 e foi encomendado como parte de um plano de reformas na igreja e nos seus edifícios conventuais pelo patrono de Leonardo, Ludovico Sforza, duque de Milão. Representa o episódio bíblico da Última Ceia de Jesus com os Apóstolos antes de ser preso e crucificado. É um dos bens culturais mais conhecidos e estimados do mundo. Estudo de Leonardo para A Última Ceia, contendo o nome de cada um dos Apóstolos. O trabalho mantém-se no convento que o sucessor de Ludovico Sforza destinou ao local de sepultura dos seus familiares. O tema da Última Ceia era tradicional em refeitórios monásticos, mas a interpretação de Leonardo deu um maior realismo e profundidade à cena representada e ao lugar Leonardo da Vinci passou grande parte destes três anos dando atenção integral a esta pintura o que era facto raro para um pintor versátil e do seu nível. Sofreu agressões ao longo do tempo desde a abertura de uma porta pelos padres até ao bombardeio aéreo na Segunda Guerra Mundial. ");
+                    }
+                
+                    function quiz_jesus() {
+                        var resposta = confirm("quer fazer o quiz?");
+                        
+                        if (resposta) {
+                            var jesus_paint = prompt("Quem fez A Ultima Ceia?");
+                            alert("o criador da Ultima Ceia foi Leonardo Davinci entre os anos 1508 e 1512");
+                        close;
+                        
+                        }
+                        }
+                        function info_venus() {
+                            alert("O Nascimento de Vênus é uma pintura de Sandro Botticelli, encomendada por Lorenzo di Pierfrancesco de Médici para a Villa Medicea di Castello.[1] E de têmpera sobre tela e mede 172,5 cm de altura por 278,5 cm de largura. A pintura representa a deusa Vênus emergindo do mar, já mulher aparentemente adulta, conforme descrito na mitologia romana. É provável que a obra tenha sido feita em 1485, sob encomenda para Lorenzo di Pierfrancesco de Médici, que a teria pedido para enfeitar sua residência, a Villa Medicea di Castello. Alguns estudiosos sugerem que a Vênus pintada para Pierfrancesco, e mencionada por Giorgio Vasari, teria sido outra que não a obra exposta em Florença e estaria perdida até o momento. Alguns historiadores acreditam que a obra seja homenagem ao amor de Juliano de Médici (que morreu em 1478, na Conspiração dos Pazzi) por Simonetta Vespúcio,[2] que viveu em Portovenere, uma cidadela à beira-mar. Qualquer que tenha sido a inspiração do artista, parecem haver influências de obras como Metamorfoses e Fasti, ambas de Ovídio, e Versos, de Poliziano. No quadro, a deusa clássica Vênus emerge das águas em uma concha, sendo empurrada para a margem por Zéfiro, o Vento Oeste, símbolos das paixões espirituais, e recebendo, de uma Hora (as Horas eram as deusas das estações), uma manto bordado de flores. Alguns especialistas argumentam que a deusa nua não representaria a paixão terrena, carnal, e sim a paixão espiritual. Apresenta-se de forma similar a antigas estátuas de mármore (cujo candor teria inspirado o escultor do século XVIII Antonio Canova), esguia e com longos membros e traços harmoniosos. O efeito causado pelo quadro, no entanto, foi um de paganismo, já que foi pintado em época em que a maioria da produção artística se atinha a temas católicos. Por isso, chega a ser surpreendente que o quadro tenha escapado das fogueiras de Savonarola, que consumiram outras tantas obras de Botticelli que teriam influências pra Vênus, assim como vários outros detalhes menores, não revela o estrito realismo clássico de Leonardo da Vinci ou Rafael. O pescoço é irrealisticamente longo e o ombro esquerdo posiciona-se em ângulo anatomicamente improvável. Não se sabe se tais detalhes constituíram erros artísticos ou licença artística, mas não chegam a atrapalhar a beleza da obra, e alguns chegam a sugerir que seriam presságios do vindouro Maneirismoـ. ");
+                            }
+                        
+                            function quiz_venus() {
+                                var resposta = confirm("quer fazer o quiz?");
+                                
+                                if (resposta) {
+                                    var venus_paint = prompt("Quem fez O nascimento de Venus?");
+                                    alert("o criador do Nascimento de Venus foi Sandro Botticelli perto do ano 1483");
+                                close;
+                                
+                                }
+                                }
+                                function info_mad() {
+                                    alert("A Madona Sistina, também chamada Madonna di San Sisto, é uma pintura a óleo realizada pelo artista italiano Rafael Sanzio. A peça-de-altar foi encomendada em 1512 pelo Papa Júlio II para a igreja de São Sisto em Placência. A pintura foi uma das últimas Madonas feitas pelo artista. Giorgio Vasari comentou que o quadro era um trabalho verdadeiramente raro e extraordinário Realocada para Dresden em 1754, a famosa pintura foi particularmente influente na Alemanha. Após a Segunda Guerra Mundial, ela foi enviada para Moscou e lá permaneceu por uma década antes de retornar à Alemanha. É agora uma obra-prima do acervo da Pinacoteca dos Mestres Antigos.  ");
+                                    }
+                                
+                                    function quiz_mad() {
+                                        var resposta = confirm("quer fazer o quiz?");
+                                        
+                                        if (resposta) {
+                                            var mad_paint = prompt("Quem fez A Madona Sistina?");
+                                            alert("o criador de A Madona Sistina foi Rafael Sanzio perto do ano 1512");
+                                        close;
+                                        
+                                        }
+                                        }
